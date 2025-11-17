6bdf610b83c950e5c0990bc381841c3978ae50e8
Dominik Hoch <dominik.hoch.1989@freenet.de>
2025-11-17 21:51:06 +0100

    added mqtt-routine, removed pi-specific libs for pyproject.toml

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import _lib.logger as lg
+from _lib.Config import Config
+import logging
+from Mqtt import Mqtt_Routine
+
 def main():
-    print("Hello from autorun-deskpi!")
+    config = Config("/Users/dom/temp/config_deskpi.txt")
+
+    lg.setup_logging(config.get_item('PWDprot'), "", add_date_to_name=True)
+
+    logging.info("Hello from autorun-deskpi!")
+
+    MR = Mqtt_Routine()
+    MR.start()
+
+
 
 
 if __name__ == "__main__":
