b2a18dbd935377d434f9e1fbe9e4c8fe009545d3
Dominik Hoch <dominik.hoch.1989@freenet.de>
2025-11-18 20:17:10 +0100

    add missing files

---
+++ Mqtt.py
@@ -0,0 +1,17 @@
+import threading
+from DataExchange import Data
+import paho.mqtt.publish as publish
+from time import sleep
+
+class Mqtt_Routine(threading.Thread):
+    def __init__(self):
+        super(Mqtt_Routine, self).__init__()
+        self.MQTT_SERVER = "192.168.178.100"
+        self.MQTT_PATH = "test_channel"
+
+    
+    
+    def run(self):
+        while True:            
+            publish.single(self.MQTT_PATH, "Hello World!", hostname=self.MQTT_SERVER)
+            sleep(5)
