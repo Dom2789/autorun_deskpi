b87a019a59cde7eb50064a3f215b18b10428070e
dom <dom_mini@Mini-von-dom.fritz.box>
2025-11-18 20:53:24 +0100

    prepared Mqtt-routine to send room data

--- src/Mqtt.py
+++ src/Mqtt.py
@@ -1,18 +1,28 @@
 import threading
 from .DataExchange import Data
 import paho.mqtt.publish as publish
-from time import sleep
+from time import sleep, strftime
 
 class Mqtt_Routine(threading.Thread):
-    def __init__(self):
+    def __init__(self, broker_IP:str, topic:str):
         super(Mqtt_Routine, self).__init__()
-        self.MQTT_SERVER = "192.168.178.100"
-        self.MQTT_PATH = "test_channel"
+        self.MQTT_SERVER = broker_IP 
+        self.MQTT_PATH = topic
 
     
     
     def run(self):
-        while True:    
-            print("send")        
-            publish.single(self.MQTT_PATH, "Hello World!", hostname=self.MQTT_SERVER)
+        while True:   
+            timestamp = strftime("%H:%M:%S")
+            data = self.data.get_data()
+            if data is None:
+                temp = 0.0
+                pres = 0.0
+                humi = 0.0
+            else:
+                temp, pres, humi = data
+
+            string = f"[{timestamp}] [{temp:.2f}C] [{pres:.2f}hPa] [{humi:.2f}%]"  
+
+            publish.single(self.MQTT_PATH, string, hostname=self.MQTT_SERVER)
             sleep(5)
